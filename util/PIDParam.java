96e6b17a195a419bfdf76022ee86a913236a2b47
Ryan Fitz-Gerald <ryan.f@pariusa.com>
2024-10-22 09:43:10 -0400

    Moved FFParam and PIDParam into lib2960

---
+++ util/PIDParam.java
@@ -0,0 +1,13 @@
+package frc.lib2960.Util;
+
+public class PIDParam {
+    public double kP;
+    public double kI;
+    public double kD;
+
+    public PIDParam(double kP, double kI, double kD) {
+        this.kP = kP;
+        this.kI = kI;
+        this.kD = kD;
+    }
+}
